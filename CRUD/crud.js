0e5f1684ec087e506aadfe8e2218a0edd73c948f
mithu2001 <guptamithu2019@gmail.com>
2022-09-20 07:48:43 +0530

    adding file system and add html code in nodejs

---
+++ CRUD/crud.js
@@ -0,0 +1,43 @@
+// Common use for the File System module:
+/*
+Read files   The fs.readFile() method is used to read files on your computer
+Create files
+Update files
+Delete files
+Rename files
+*/
+// var http = require('http');
+// var fs = require('fs');
+// http.createServer(200,function(req,res){
+//     res.writeHead(200,{'content-type':'text/html'});
+//     res.write("The Message comes from server");
+
+
+//     res.end();
+// }).listen(8080);
+
+// var http = require('http');
+// var formidable = require('formidable');
+// var fs = require('fs');
+
+// http.createServer(function (req, res) {
+//   if (req.url == '/fileupload') {
+//     var form = new formidable.IncomingForm();
+//     form.parse(req, function (err, fields, files) {
+//       var oldpath = files.filetoupload.filepath;
+//       var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+//       fs.rename(oldpath, newpath, function (err) {
+//         if (err) throw err;
+//         res.write('File uploaded and moved!');
+//         res.end();
+//       });
+//  });
+//   } else {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+//     res.write('<input type="file" name="filetoupload"><br>');
+//     res.write('<input type="submit">');
+//     res.write('</form>');
+//     return res.end();
+//   }
+// }).listen(8080);
--- myfirst.js
+++ myfirst.js
@@ -1,10 +1,18 @@
 var http = require('http'); //imported http built in module
 var dt = require('./myfirstmodule'); //imported module
+var fs = require('fs');
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'});//The first argument of the res.writeHead() method is the status code, 
                              //200 means that all is OK, the second argument is an object containing the response headers.
     
-    res.write("The current time and date is :"+dt.myDateTime());//Write a Response for Client
+    //Write a Response for Client
+    res.write(`
+    <h1 style="color:red;">Hello India</h1><br/>
+    <input type="button">
+    The current time and date is :-${dt.myDateTime()}
+    `
+    
+    );
     res.end();//end the response
     
 }).listen(8000);//the server object listens on port 8000
